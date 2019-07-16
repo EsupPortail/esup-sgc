f1db90ed6b9a9663ac53af0ca17adfd549ff2ad8
Vincent Bonamy <vbonamy@gmail.com>
2019-07-16 15:04:25 +0200

    MultiGroupService

---
+++ src/main/java/org/esupportail/sgc/services/ldap/MultiGroupService.java
@@ -0,0 +1,36 @@
+package org.esupportail.sgc.services.ldap;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class MultiGroupService implements GroupService {
+	
+	private List<GroupService> groupServices;
+
+	public void setGroupServices(List<GroupService> groupServices) {
+		this.groupServices = groupServices;
+	}
+
+	@Override
+	public List<String> getGroupsForEppn(String eppn) {	
+		Set<String> groups = new HashSet<String>();	
+		for(GroupService groupService: groupServices) {
+			groups.addAll(groupService.getGroupsForEppn(eppn));
+		}	
+		return new ArrayList<String>(groups);
+		
+	}
+
+	@Override
+	public List<String> getMembers(String groupName) {
+		Set<String> members = new HashSet<String>();
+		for(GroupService groupService: groupServices) {
+			members.addAll(groupService.getMembers(groupName));
+		}	
+		return new ArrayList<String>(members);
+	}
+	
+}
+
