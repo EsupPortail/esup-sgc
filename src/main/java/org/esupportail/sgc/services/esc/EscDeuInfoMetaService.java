726e7cec9b84ef72751e963eb8d5622d493549ae
Vincent Bonamy <vbonamy@gmail.com>
2021-06-08 10:37:31 +0200

    Debug : EscDeuInfoService bean is optional

--- src/main/java/org/esupportail/sgc/services/esc/EscDeuInfoMetaService.java
+++ src/main/java/org/esupportail/sgc/services/esc/EscDeuInfoMetaService.java
@@ -28,6 +28,7 @@ import org.esupportail.sgc.domain.Card;
 import org.esupportail.sgc.exceptions.SgcRuntimeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 
@@ -36,7 +37,7 @@ public class EscDeuInfoMetaService  {
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
 
-	@Resource
+	@Autowired(required = false)
 	List<EscDeuInfoService> escDeuInfoServicesList;
 		
 	@Resource
