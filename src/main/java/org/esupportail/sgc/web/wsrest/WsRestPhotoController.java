b8c0b4b26c40dbfd57f19070c235539984341259
Vincent Bonamy <vbonamy@gmail.com>
2024-03-15 10:42:12 +0100

    Revert "API REST : Controller->RestController"
    This reverts commit b8e1d0bef184fb0e5ece30dfee4ed750ab3c579c.

--- src/main/java/org/esupportail/sgc/web/wsrest/WsRestPhotoController.java
+++ src/main/java/org/esupportail/sgc/web/wsrest/WsRestPhotoController.java
@@ -27,11 +27,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Transactional
 @RequestMapping("/wsrest/photo")
-@RestController
+@Controller
 public class WsRestPhotoController extends AbstractRestController {
 	
 	private final Logger log = LoggerFactory.getLogger(getClass());
